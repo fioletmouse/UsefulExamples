6cef4c26285fc16e3bd0cb35a9403f6838e4f3a0
fioletmouse <logika2003@inbox.ru>
2016-04-10 19:59:29 +0600

    New project

---
+++ UsefulExamples/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UsefulExamples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
